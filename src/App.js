f0ad5975e446016e291b3c8f8ac28af347dcf938
alamin-merndev <alaminnishat@gmail.com>
2022-11-17 16:57:20 +0600

    Login & Register Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./Routes/Routes/Routes";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,50 +1,66 @@
-import { GoogleAuthProvider } from "firebase/auth";
 import React, { useContext, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Icon, Input } from "semantic-ui-react";
 import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 import TittleHeader from "../Shared/TittleHeader/TittleHeader";
-import { FcGoogle } from "react-icons/fc";
-import { Button, Icon, Input } from "semantic-ui-react";
-import "./Login.css";
-import { toast, ToastContainer } from "react-toastify";
 
-const Login = () => {
+const Register = () => {
   const [error, setError] = useState("");
-  const { signIn, setLoading, providerLogin, logOut } = useContext(AuthContext);
+
+  const { createUser, updateUserProfile, verifyEmail, logOut } =
+    useContext(AuthContext);
+
   const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
 
-  const googleProvider = new GoogleAuthProvider();
+  const handleUpdateUserProfile = (name, photoURL) => {
+    const profile = {
+      displayName: name,
+      photoURL: photoURL,
+    };
 
-  const handleLogin = (event) => {
+    updateUserProfile(profile)
+      .then(() => {})
+      .catch((error) => console.error(error));
+  };
+
+  const handleLogout = () => {
+    logOut()
+      .then((res) => {})
+      .catch((error) => {});
+  };
+
+  const handleRegister = (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.image.value;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
-    signIn(email, password)
+
+    createUser(email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
-        form.reset();
         setError("");
-        toast.success("Successfully logged in!");
+        form.reset();
+        handleUpdateUserProfile(name, photoURL);
+        toast.success("Please Login your account please.");
+
         setTimeout(() => {
-          navigate(from, { replace: true });
-        }, 2000);
+          navigate("/login");
+          handleLogout();
+        }, 1000);
       })
-      .catch((error) => {
-        console.error(error);
-        setError(error.message);
-      })
-      .finally(() => {
-        setLoading(false);
+      .catch((e) => {
+        console.error(e);
+        setError(e.message);
       });
   };
   return (
     <div>
-      <TittleHeader title={"Login"}></TittleHeader>
+      <TittleHeader title={"Sign Up"}></TittleHeader>
       <section className="w-1/2 my-12 mx-auto rounded shadow-lg">
         <div className="grid grid-cols-2 ">
           <div
@@ -52,17 +68,40 @@ const Login = () => {
               backgroundImage: `url("https://i.ibb.co/b1nFf4x/professional-programmer-working-late-dark-office.jpg")`,
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
+
               overflow: "hidden",
             }}
           ></div>
           <div className="mx-auto">
-            <form onSubmit={handleLogin} className="py-12 mx-auto">
+            <form onSubmit={handleRegister} className="py-12 mx-auto">
+              <Input
+                className=""
+                iconPosition="left"
+                placeholder="Name"
+                name="name"
+                size="big"
+              >
+                <Icon name="user" />
+                <input />
+              </Input>
+              <br />
+              <br />
+              <Input
+                iconPosition="left"
+                placeholder="Photo Url"
+                name="image"
+                size="big"
+              >
+                <Icon name="image" />
+                <input />
+              </Input>
+              <br />
+              <br />
               <Input
                 iconPosition="left"
                 placeholder="Email"
                 name="email"
                 size="big"
-                required
               >
                 <Icon name="at" />
                 <input />
@@ -81,18 +120,17 @@ const Login = () => {
               </Input>
               <br />
               <br />
-              <p>{error}</p>
 
               <div className="text-center">
                 <button className="btn btn-info text-white" type="submit">
-                  Login
+                  Sign Up
                 </button>
               </div>
               <p className="my-5 text-center">
-                New To This Site?{" "}
-                <Link className="login-a" to="/register">
+                Already have an account?{" "}
+                <Link className="login-a" to="/login">
                   {" "}
-                  Create an account
+                  Please Login
                 </Link>
               </p>
               <div className="or-text-box">
@@ -101,18 +139,6 @@ const Login = () => {
               <div className="social-login mt-5 text-center">
                 <FcGoogle className="inline fa-login" size={"2em"}></FcGoogle>
               </div>
-              <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"
-              />
             </form>
           </div>
         </div>
@@ -121,4 +147,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
